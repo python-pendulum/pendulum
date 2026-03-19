b6ef48d437809b4b7a498241fa1f99ebb53ecb0f
Ayush Kumar <ayush438500@gmail.com>
2026-03-19 17:19:40 +0530

    Create tests for parse_date()

--- tests/parsing/test_strict_parse.py
+++ tests/parsing/test_strict_parse.py
@@ -7,6 +7,8 @@ from pendulum.parser import (
     parse_duration,
 )
 
+# tests for parse_datetime
+
 def test_parse_datetime_valid() -> None:
     dt = parse_datetime("2026-03-19T11:28:37")
     assert dt.year == 2026
@@ -18,3 +20,13 @@ def test_parse_datetime_rejects_date() -> None:
 def test_parse_datetime_rejects_interval() -> None:
     with pytest.raises(ValueError):
         dt = parse_datetime("2026-03-19T12:00:00/2026-03-19T13:00:00")
+
+# tests for parse_date
+
+def test_parse_date_valid() -> None:
+    dt = parse_date("2026-03-19")
+    assert dt.year == 2026
+
+def test_parse_date_rejects_datetime() -> None:
+    with pytest.raises(ValueError):
+        dt = parse_date("2026-03-19T11:28:37")
