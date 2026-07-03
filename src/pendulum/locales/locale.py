d07a06c91e94a2eb1c84cb6ffaf6bf8d3704f9de
Denys Nykula <nykula@ukr.net>
2026-07-03 14:47:31 +0300

    Keep only 'uk', not 'ua', as discussed with @Secrus

--- src/pendulum/locales/locale.py
+++ src/pendulum/locales/locale.py
@@ -53,11 +53,7 @@ class Locale:
         if m:
             return f"{m.group(1).lower()}_{m.group(2).lower()}"
         else:
-            lower = locale.lower()
-            if lower == 'ua':
-                # Backward compatibility. Ukrainian was 'ua' initially.
-                lower = 'uk'
-            return lower
+            return locale.lower()
 
     def get(self, key: str, default: Any | None = None) -> Any:
         if key in self._key_cache:
